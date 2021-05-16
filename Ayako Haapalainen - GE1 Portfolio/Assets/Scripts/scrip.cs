beaa67d9449b8d5ae7751fbb7095c4dfb73b247e
AA2234 <aa2234@student.jamk.fi>
2021-05-16 16:11:37 +0300

    Initial commit

---
+++ Ayako Haapalainen - GE1 Portfolio/Assets/Scripts/scrip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float degrees = 90;
+        float degrees2 = 90;
+        Vector3 to = new Vector3(degrees, 0, degrees2);
+
+        transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime);
+    }
+}
